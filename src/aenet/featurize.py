dc5af6d1377ccce06ffb68546e6b9381c20a2d92
Koen Draijer <124170375+KMdraijer@users.noreply.github.com>
2025-10-22 10:56:14 +0200

    Add forcespercent parameter to featurize function
    Modification to function `generate_input_string` in  `src/aenet/featurize.py` to allow specification of the percent of forces for aenet-pytorch training. The parameter `forcespercent` will default to None, so no other functionality is compromised.

--- src/aenet/featurize.py
+++ src/aenet/featurize.py
@@ -100,6 +100,7 @@ class AenetAUCFeaturizer(AtomicFeaturizer):
                               deriv_outfile: str = 'data.deriv.train',
                               deriv_disp_all: bool = True,
                               deriv_N_max: int = 100000,
+                              forcespercent: int = None,
                               **kwargs):
 
         for arg in kwargs:
@@ -153,6 +154,9 @@ class AenetAUCFeaturizer(AtomicFeaturizer):
         if debug:
             generate_in += "\nDEBUG\n"
 
+        if forcespercent is not None:
+            generate_in += "\nFORCES\nforcespercent\n{}\n".format(forcespercent)
+    
         generate_in += "\nFILES\n{}\n".format(len(xsf_file_paths))
         for p in xsf_file_paths:
             generate_in += "{}\n".format(p)
